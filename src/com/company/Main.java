c4e29b9392ad045083f9b19f6dea7fea55c2ae13
Alesia Kolbik <moyaalesia@mail.ru>
2017-10-14 12:31:54 +0300

    refactor code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int n=(int)(Math.random()*25+1000);
-        {
-            if (n % 10 == 1) {
-                System.out.println(+n + " рубль");}
-            if (n % 10 >= 2 && n % 10 <= 4 ) {
-                System.out.println(+n + " рубля");}
-             if(n%10>=5 && n%10<=19 || n%10==0){
-                System.out.println(+n+" рублей");}
-            }
+
+        int amountOfMoney = 2;//(int) (Math.random() * Integer.MAX_VALUE);
+
+        int firstDigit = amountOfMoney % 10;
+        int secondDigit = (amountOfMoney / 10) % 10;
+        int value = secondDigit * 10 + firstDigit;
+
+        if (value >= 5 && value <= 19 || firstDigit == 0) {
+            System.out.println(+amountOfMoney + " рублей");
+        } else if (firstDigit >= 2 && firstDigit <= 4) {
+            System.out.println(+amountOfMoney + " рубля");
+        } else if (firstDigit == 1) {
+            System.out.println(+amountOfMoney + " рубль");
+        } else {
+            System.out.println(+amountOfMoney + " рублей");
         }
     }
+}
